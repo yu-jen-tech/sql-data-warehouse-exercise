6a530f11e9e5f650637bb27983cd6f196fc8eaa7
Tony <yu.jen.chang.tech@gmail.com>
2025-08-15 11:19:11 +0200

    feat(schema): extend silver.crm_prd_info with new columns and data type changes

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -31,12 +31,13 @@ GO
 
 CREATE TABLE silver.crm_prd_info (
     prd_id       INT,
+    cat_id       NVARCHAR(50),
     prd_key      NVARCHAR(50),
     prd_nm       NVARCHAR(50),
     prd_cost     INT,
     prd_line     NVARCHAR(50),
-    prd_start_dt DATETIME,
-    prd_end_dt   DATETIME,
+    prd_start_dt DATE,
+    prd_end_dt   DATE,
     dwh_create_date    DATETIME2 DEFAULT GETDATE()
 );
 GO
